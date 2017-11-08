e28754bdbe1dea6232ca1bb9d9d754073876256b
Doonyby <doonyby@gmail.com>
2017-11-08 16:52:03 -0700

    finished product detail http call and template

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,24 +10,31 @@ import { IProduct } from './product';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
+  errorMessage: string;
   product: IProduct;
 
   constructor(private _route: ActivatedRoute,
-              private _router: Router) { }
+              private _router: Router,
+              private _productService: ProductService) { }
 
   ngOnInit() {
     let id = +this._route.snapshot.paramMap.get('id');
+    this._productService.getProduct(id).subscribe(product => {
+                                                  this.product = product;
+                                                  console.log('product', this.product);
+                                                  }, error => this.errorMessage = <any>error);
+    
     this.pageTitle += `: ${id}`;
-    this.product = {
-      "productId": id,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2016",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "http://openclipart.ort/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png",
-    }
+    // this.product = {
+    //   "productId": id,
+    //   "productName": "Leaf Rake",
+    //   "productCode": "GDN-0011",
+    //   "releaseDate": "March 19, 2016",
+    //   "description": "Leaf rake with 48-inch wooden handle.",
+    //   "price": 19.95,
+    //   "starRating": 3.2,
+    //   "imageUrl": "http://openclipart.ort/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png",
+    // }
   }
 
   onBack(): void {
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import { IProduct } from './product';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
 
 
 @Injectable()
@@ -13,12 +14,18 @@ export class ProductService {
 
     getProducts(): Observable<IProduct[]> {
         return this._http.get<IProduct[]>(this._productUrl)
-            .do(data => console.log('All: ' + JSON.stringify(data)))
+            // .do(data => console.log('All: ' + JSON.stringify(data)))
             .catch(this.handleError);
+    }
+
+    getProduct(id: number): Observable<IProduct> {
+        return this.getProducts()
+            .map((products: IProduct[]) => products.find(p => p.productId === id));
     }
 
     private handleError(err: HttpErrorResponse) {
         console.log(err.message);
         return Observable.throw(err.message);
     }
+
 }
