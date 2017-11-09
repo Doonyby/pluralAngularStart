ede563ba5958d8779bdb599ded279dbb9fafe7e9
Doonyby <doonyby@gmail.com>
2017-11-09 16:50:54 -0700

    separated app into different modules for separation of concerns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { ProductService } from './products/product.service';
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  providers: [ProductService],
 })
 
 export class AppComponent {
